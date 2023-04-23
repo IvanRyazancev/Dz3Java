bf1400c9e06907040fe180e3722c31a6e18bd955
ryazancev.azov <ryazancev.azov@gmail.com>
2023-04-23 23:36:08 +0300

    Задача №1 решена

--- Main.java
+++ Main.java
@@ -1,18 +1,40 @@
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
 
-        ex01();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество элементов: ");
+        int num = in.nextInt();
+        ex01(num);
         ex02();
         ex03();
     }
 
-    static void ex01(){
+    static void ex01(int num){
 
         // Пусть дан произвольный список целых чисел, удалить из него четные числа
 
+        Integer[] randomArr = new Integer[num];
+        Random rndGen = new Random();
+        for (int i = 0; i < randomArr.length; i++) {
+            randomArr[i] = rndGen.nextInt(100);
+        }
+        System.out.println("Исходный массив: " + Arrays.toString(randomArr));
 
+        Integer[] newArr = new Integer[num];
+        for (int i = 0; i < randomArr.length; i++) {
+            if (randomArr[i] % 2 == 0 && randomArr[i] != 0) {
+                newArr[i] = randomArr[i];
+            }
+        }
+        newArr = Arrays.stream(newArr).filter(Objects::nonNull).toArray(Integer[]::new);
+        System.out.println("Обработанный массив: " + Arrays.toString(newArr));
+        }
 
-    }
 
     static void ex02(){
 
